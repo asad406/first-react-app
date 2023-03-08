f513ef5c185b04a2a9b6e3dc48a443d569e64a8a
asad406 <asadl76@gmail.com>
2023-03-08 09:15:27 +0600

    Mapping data to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Card from './components/Card'
+import Data from './data.json'
 
 function App() {
     return <div>
         <h1 className="headingStyle">Todo App</h1>
-        <Card titleText = "Call Father" decText = 'lorm impusn not myoy noryl mi anoy audiu'/>
-        <Card titleText = "Call Mother" decText = 'lorm impusn not myoy noryl mi anoy audiu'/>
-        <Card titleText = "Call Wife" decText = 'lorm impusn not myoy noryl mi anoy audiu'/>
+        <Card titleText = {Data[0].title} decText = {Data[0].desc}/>
+        <Card titleText = {Data[1].title} decText = {Data[1].desc}/>
+        <Card titleText = {Data[2].title} decText = {Data[2].desc}/>
+        
         </div>
 
 }
