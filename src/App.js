1209299baadc12d2702c8eea269f64f05736b588
asad406 <asadl76@gmail.com>
2023-03-08 10:01:23 +0600

    json data add in card by for loop

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Card from './components/Card'
 import Data from './data.json'
 
 function App() {
-    return <div>
-        <h1 className="headingStyle">Todo App</h1>
-        <Card titleText = {Data[0].title} decText = {Data[0].desc}/>
-        <Card titleText = {Data[1].title} decText = {Data[1].desc}/>
-        <Card titleText = {Data[2].title} decText = {Data[2].desc}/>
+    let items = [];
+    for (let x = 0; x < Data.length; x++) {
+        items.push(<Card titleText = {Data[x].title} decText = {Data[x].desc}/>)
         
+    }
+    return <div>
+        <h1 className="headingStyle">Todo App</h1>        
+        {items}
         </div>
 
 }
