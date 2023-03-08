4a8c2fe51eea8f89e0ea483b077ba950123eeaa0
asad406 <asadl76@gmail.com>
2023-03-08 10:08:33 +0600

    json data add in card by map()

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Data from './data.json'
 
 function App() {
     let items = [];
-    for (let x = 0; x < Data.length; x++) {
-        items.push(<Card titleText = {Data[x].title} decText = {Data[x].desc}/>)
-        
-    }
+  /*   for (let x = 0; x < Data.length; x++) {
+        items.push(<Card titleText = {Data[x].title} decText = {Data[x].desc}/>)        
+    } */
+    items = Data.map((item) => <Card titleText = {item.title} decText = {item.desc}/>)
     return <div>
         <h1 className="headingStyle">Todo App</h1>        
         {items}
